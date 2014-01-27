482bece32f94ec1fc7db92df1207a5c2c1536bb6
Corey Richardson <corey@octayn.net>
2014-01-26 23:25:54 -0500

    Site updated at 2014-01-27 04:25:54 UTC

--- javascripts/slash.js
+++ javascripts/slash.js
@@ -1,19 +1,4 @@
 (function($){
-	// Open external links in new window
-	var externalLinks = function(){
-		var host = location.host;
-
-		$('body').on('click', 'a', function(e){
-			var href = this.href,
-				link = href.replace(/https?:\/\/([^\/]+)(.*)/, '$1');
-
-			if (link != '' && link != host && !$(this).hasClass('fancybox')){
-				window.open(href);
-				e.preventDefault();
-			}
-		});
-	};
-
 	// Append caption after pictures
 	var appendCaption = function(){
 		$('.entry-content').each(function(i){
